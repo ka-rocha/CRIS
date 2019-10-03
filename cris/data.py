Fixed the percent change calculation in data.py to find nearest neighbors in input space, then take diffs in output space. Sample.py now takes the max average percent change in all ouptut for a given class.

cris/data.py
@@ -17,7 +17,7 @@ def calc_avg_dist(data, n_neighbors, neighbor=None):
     ------
     data : ndarray
         Data to train the NearestNeighbors class on.
-    n_neighbors : int, list
+    n_neighbors : int
         Number of neighbors to use when finding average distance.
     neighbor : instance of NearestNeightbors class
         For passing your own object.
@@ -26,6 +26,8 @@ def calc_avg_dist(data, n_neighbors, neighbor=None):
     -------
     avg_dist : array
         The average distance between nearest neighbors.
+    g_indi : array
+        Indicies that correspond to the nearest neighbors.
     """
     if neighbor:
         neigh = neighbor
@@ -33,34 +35,38 @@ def calc_avg_dist(data, n_neighbors, neighbor=None):
         neigh = NearestNeighbors()
     neigh.fit(data)
 
-    if isinstance(n_neighbors, int):
-        # since we pass the original data, the first nearest point is itself
-        # if we want 2 nearest neighbors, we ask for 3 because the first will always be 0
-        dist, indicies = neigh.kneighbors( data, n_neighbors=(n_neighbors+1) )
+    # since we pass the original data, the first nearest point is itself
+    # if we want 2 nearest neighbors, we ask for 3 because the first will always be 0
+    dist, indicies = neigh.kneighbors( data, n_neighbors=(n_neighbors+1) )
 
-        g_dist = (dist.T[1:len(dist)]).T
-        g_indi = (indicies.T[1:len(indicies)]).T
-        avg_dist =  np.mean(g_dist, axis = 1)
-    else:
-        avg_dist_list = []
-        for n in n_neighbors:
-            dist, indicies = neigh.kneighbors( data, n_neighbors=(n+1) )
+    g_dist = (dist.T[1:len(dist)]).T
+    g_indi = (indicies.T[1:len(indicies)]).T
+    avg_dist =  np.mean(g_dist, axis = 1)
 
-            g_dist = (dist.T[1:len(dist)]).T
-            g_indi = (indicies.T[1:len(indicies)]).T
-            avg_dist =  np.mean(g_dist, axis = 1)
-            avg_dist_list.append(avg_dist)
-        avg_dist = np.array( avg_dist_list )
-    return avg_dist
+    return avg_dist, g_indi
 
 
-def calc_avg_p_change( data, avg_dist ):
+def calc_avg_p_change( data, where_nearest_neighbors ):
     where_zero = np.where(data==0)[0]
     if len(where_zero)>0:
         return None
     else:
-        avg_p_change = abs(avg_dist-data)/data
-        return avg_p_change
+        pass
+
+    avg_p_change_holder = []
+    for n_, indicies in where_nearest_neighbors.items():
+        diff_holder = []
+        for i in range(n_):
+            nearest_data = data[ indicies.T[i] ]
+            diff_holder.append( data - nearest_data )
+
+        diffs = np.array(diff_holder).T
+        avg_diffs = np.mean( diffs, axis=1 )
+        avg_p_change = abs(avg_diffs)/data
+
+        avg_p_change_holder.append( avg_p_change )
+
+    return np.array( avg_p_change_holder )
 
 class TableData():
     """
@@ -298,18 +304,24 @@ class TableData():
         self._n_neighbors_ = [int(i) for i in n_neighbors]
         self._avg_dist_dfs_per_class_ = dict() # stores the average distances
 
-        for key, val in self._regr_dfs_per_class_.items():
-            # key is the class, val is another dataframe or np.nan
+        # We need distances in input space, then compare %diff in output space
+        for key, val in self._regr_inputs_.items():
+            # key is the class, val is input DataFrame or np.nan
             self._avg_dist_dfs_per_class_[key] = dict()
+            regr_data = self._regr_dfs_per_class_[key]
 
-            if isinstance(val, pd.DataFrame):
+            if isinstance(regr_data, pd.DataFrame):
                 self.__vb_helper(verbose, "class: '{0}'".format(key))
 
-                for _key, _val in val.items():
-                    data = np.array(_val.values, dtype=float)
-                    correct_shape = data[np.newaxis,:].T
-                    avg_dist = calc_avg_dist( correct_shape, self._n_neighbors_, neighbor=neighbor )
+                # find nearest neighbors in input space
+                where_nearest_neighbors = dict()
+                for n_ in self._n_neighbors_:
+                    avg_dist, indicies = calc_avg_dist(val.values, n_, neighbor=neighbor)
+                    where_nearest_neighbors[n_] = indicies
 
+
+                for _key, _val in regr_data.items(): # regression data
+                    data = np.array(_val.values, dtype=float)
                     where_zero = np.where(data==0)[0]
                     if len(where_zero)>0:
                         info_str_15 = "\t -- {0} zeros in '{1}'. Skipping p_change...".format(len(where_zero),_key)
@@ -317,7 +329,7 @@ class TableData():
                         pass
                     else:
                         # Take percent difference
-                        avg_p_change = calc_avg_p_change( data, avg_dist )
+                        avg_p_change = calc_avg_p_change( data, where_nearest_neighbors )
                         if avg_p_change is None:
                             self.__vb_helper(verbose, "None in avg_p_change!? Should not happen...")
                         else:

cris/regress.py
@@ -53,17 +53,19 @@ class Regressor():
         self.__all_diffs_holder = makehash()
         self.__all_Pchange_holder = makehash()
 
-    def train_everything(self, verbose=False):
-        regressor_names = ["rbf", "linear", "gp"]
+    def train_everything(self, regressor_names, verbose=False):
+        """Train all classes and columns with the specified list of regressor names."""
         for regr_name in regressor_names:
-            print("Regressor: {0}".format(regr_name))
+            if verbose:
+                print("Regressor: {0}".format(regr_name))
             class_keys=list(self.regr_dfs_per_class.keys())
             for class_name in class_keys:
                 self.train(regr_name, [class_name], None, verbose=verbose)
+        if verbose:
+            print("\nDone Regrssor train_everything.")
 
 
     def train(self, regressor_name, class_keys, col_keys, di = None, verbose = False, train_cross_val = False ):
-
         """Train a regression algorithm.
 
         Can choose mutliple classes / columns at once as long as
@@ -77,9 +79,11 @@ class Regressor():
             first_class_data = self.regr_dfs_per_class[class_keys[0]]
             if isinstance(first_class_data, pd.DataFrame):
                 col_keys = np.array( first_class_data.keys() )
-                print("Training on all {0} columns in {1}.".format(len(col_keys),class_keys[0]) )
+                if verbose:
+                    print("\t Training on all {0} columns in '{1}'...".format(len(col_keys),class_keys[0]) )
             else:
-                print("No regression data for {0}.".format(class_keys[0]))
+                if verbose:
+                    print("No regression data for {0}.".format(class_keys[0]))
                 return
 
         if   regressor_key == "LinearNDInterpolator":
@@ -442,9 +446,6 @@ class Regressor():
         self.__all_diffs_holder[regressor_key][class_key][col_key] = {np.arange(0,Npoints):[]}
         self.__all_Pchange_holder[regressor_key][class_key][col_key] = {np.arange(0,Npoints):[]}
         percent_diffs, diffs = self.cross_validate( regressor_name, class_key, col_key, alpha, verbose = verbose)
-
-    def get_avg_diffs(self, regressor_name, class_key, col_key, alpha, verbose = False):
-        return avg_diffs
 
     def mult_diffs(self, regressor_name, class_key, col_keys, alpha, cutoff, verbose = False):
 

cris/sample.py
@@ -9,22 +9,26 @@ import scipy.stats
 class Sampler():
     """Add some docstrings"""
 
-    def __init__( self, classifier, regressor = None ):
+    def __init__( self, classifier = None, regressor = None ):
         self._Classifier_ = classifier
         self._Regressor_ = regressor
 
-        # Find the bounds of the walker - should be TableData attribute
-        self.max_vals = []
-        self.min_vals = []
-        input_data_cols = self._Classifier_.table_data.get_input_data().T
-        for data in input_data_cols:
-            self.max_vals.append(max(data))
-            self.min_vals.append(min(data))
-        # self._max_vals_, self._min_vals_ = self._Classifier_.get_data_bounds()
+        if self._Classifier_ is None:
+            pass
+        else:
+            # Find the bounds of the walker - should be TableData attribute
+            self.max_vals = []
+            self.min_vals = []
+            input_data_cols = self._Classifier_._TableData_.get_data(what_data='input').T
+            for data in input_data_cols:
+                self.max_vals.append(max(data))
+                self.min_vals.append(min(data))
+            # self._max_vals_, self._min_vals_ = self._Classifier_.get_data_bounds()
 
         # You can save chains_history in here
         self._chain_step_hist_holder_ = dict()
 
+        self._MAX_APC_str_ = []
 
     def analytic_target_dist( self, name, args ):
         """For testing."""
@@ -33,25 +37,58 @@ class Sampler():
         arg2 = - 8*mu**2 - 8*( nu - 2 )**2
         return (16)/(3*np.pi) * ( np.exp(arg1) + 0.5*np.exp(arg2) )
 
-    def classifier_target_dist( self, classifier_name, args  ):
+    def classifier_target_dist( self, classifier_name, args, other_args=None ):
         """Target distribution using classification only."""
-        correct_shape_args = np.array( [args] )
-        max_probs, nan_locs = self._Classifier_.return_probs( classifier_name, correct_shape_args, \
+        if other_args is None:
+            correct_shape_args = np.array( [args] )
+        else:
+            correct_shape_args = other_args
+
+        normalized_probs, where_not_nan = self._Classifier_.return_probs( classifier_name, correct_shape_args, \
                                                               verbose=False )
-        if len(nan_locs) > 0:
+
+        max_probs = np.max(normalized_probs, axis=1)
+        if (len(where_not_nan) != len(max_probs)):
             return 0
         else:
             return (1-max_probs)
 
-    def cls_rgr_target_dist(self, names, args):
+    def cls_rgr_target_dist(self, names, args, other_args = None):
         """This isn't even a function in regressor, this is what we need to do"""
-        correct_shape_args = np.array( [args] )
-        max_probs, nan_locs = self._Classifier_.return_probs( names[0], correct_shape_args, all_probs=False )
-        # The second return of return_probs is a list filled with nan_locations
-        if len(nan_locs) > 0:
+        if other_args is None:
+            correct_shape_args = np.array( [args] )
+        else:
+            correct_shape_args = other_args
+
+        normalized_probs, where_not_nan = self._Classifier_.return_probs( names[0], correct_shape_args, \
+                                                              verbose=False )
+        max_probs = np.max(normalized_probs, axis=1)
+        pred_class_id = np.argmax(normalized_probs[0])
+        cls_key = self._Classifier_.class_id_mapping[ pred_class_id ]
+
+        if (len(where_not_nan) != len(max_probs)):
             return 0
-        #regr_error, nan_locs = self._Regressor_.return_errors( names[1], correct_shape_args )
-        return 0
+        else:
+            if isinstance(self._Regressor_.regr_dfs_per_class[cls_key], pd.DataFrame):
+                pass
+            else:
+                # nan value, do nothing
+                return (1-max_probs) + 0
+
+            this_class_regr_cols = self._Regressor_.regr_dfs_per_class[cls_key].keys()
+            good_col_keys = [i for i in this_class_regr_cols if "APC" in i] # columns with average percent change data
+
+            predictions = self._Regressor_.get_predictions( [names[1]], [cls_key], good_col_keys, correct_shape_args  )
+            regr_key = self._Regressor_.get_regressor_name_to_key(names[1])
+
+            dict_with_APC_data = predictions[regr_key][cls_key]
+            max_APC_vals = [i[0] for i in dict_with_APC_data.values()]
+            max_APC = np.max(max_APC_vals)
+            which_col_max = list(dict_with_APC_data.keys())[np.argmax(max_APC_vals)]
+
+            self._MAX_APC_str_.append(which_col_max)
+
+            return (1-max_probs) + max_APC
 
 
     def save_chain_step_history(self, key, chain_step_history, overwrite=False):
@@ -129,7 +166,7 @@ class Sampler():
         acc_ratio_holder = np.zeros( num_chains )
 
         start_time = time.time()
-        for k in range(N_loops):
+        for counter in range(N_loops):
             # Number of draws before swap
             N_draws = N_draws_per_swap
 
@@ -154,7 +191,7 @@ class Sampler():
 
             if verbose:
                 #print( "acc/total: {0}".format(acc_ratio_holder[0]), end="\r" )
-                b = "num_acc/total: Tmax {0:.4}, Tmin {1:.4}, k {2}".format( acc_ratio_holder[0], acc_ratio_holder[-1], k )
+                b = "num_acc/total: Tmax {0:.4}, Tmin {1:.4}, loop # {2}".format( acc_ratio_holder[0], acc_ratio_holder[-1], counter )
                 sys.stdout.write('\r'+b)
 
             # Calc H to see if chains SWAP
@@ -445,7 +482,7 @@ class Sampler():
         fig, axs = plt.subplots(nrows=n_axis, ncols=2, \
                                        figsize=(10,5), dpi=100, \
                                        gridspec_kw={'width_ratios':[1.8, 1]})
-        axis_names = self._Classifier_.table_data.get_input_data(return_df=True).keys()
+        axis_names = self._Classifier_._TableData_.get_data(what_data='input',return_df=True).keys()
         for num, ax in enumerate(axs):
             ax[0].plot(steps[num], '-', linewidth=0.5,color = "C4")
             ax[0].set_title( "Input: {0}".format(axis_names[num]) )