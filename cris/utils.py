ad26327735113c19fcdda9a93a003280b232440f
Kyle Rocha <kylerocha@pa-dhcp-10-120-232-225.gp-vpn.northwestern.private>
2020-05-28 14:15:52 -0500

    Added grid and random df generation functions

--- cris/utils.py
+++ cris/utils.py
@@ -65,6 +65,9 @@ DEFAULT_ALL_KWARGS = {
     "Propose_kwargs":DEFAULT_PROPOSE_KWARGS,
     }
 
+def get_all_default_kwargs():
+    return DEFAULT_ALL_KWARGS
+
 def get_new_query_points( N_new_points=1, TableData_kwargs = {},
                                   Classifier_kwargs={}, Regressor_kwargs={},
                                   Sampler_kwargs={}, Propose_kwargs={}, threshold=1e-5 ):
@@ -102,8 +105,11 @@ def get_new_query_points( N_new_points=1, TableData_kwargs = {},
     cls_obj.train_everything(**Classifier_kwargs)
 
     # Regressor
-    regr_obj = Regressor(table_obj)
-    regr_obj.train_everything(**Regressor_kwargs)
+    if Regressor_kwargs is not None:
+        regr_obj = Regressor(table_obj)
+        regr_obj.train_everything(**Regressor_kwargs)
+    else:
+        regr_obj = None
 
     # Sampler
     sampler_obj = Sampler( classifier = cls_obj, regressor = regr_obj )
@@ -111,18 +117,13 @@ def get_new_query_points( N_new_points=1, TableData_kwargs = {},
     target_dist_name = Sampler_kwargs.get("target_dist", "TD_classification")
     target_dist_obj =  eval( "sampler_obj.{0}".format(target_dist_name) )
     Sampler_kwargs['target_dist'] = target_dist_obj
-    # Sampler.run_PTMCMC
-    if Sampler_kwargs['target_dist']("linear", Sampler_kwargs['init_pos'])[0] == 0:
-        print("\t !!!!! ZERO for target dist !!!!!")
-        return None, None
+
     chain_step_history, T_list = sampler_obj.run_PTMCMC(**Sampler_kwargs)
     last_chain_hist = chain_step_history[len(T_list)-1]
 
-    proposed_above_thresh = check_dist( table_obj.get_data(what_data="input"), last_chain_hist, threshold=threshold )
-    if np.sum(proposed_above_thresh) < len(last_chain_hist):
-        # if points in the chain history are close to accepted points
-        print("\t Removed {} points from posterior.".format(np.sum(proposed_above_thresh)))
-        last_chain_hist = last_chain_hist[proposed_above_thresh]
+    # burn in - default to use entire chain
+    where_to_cut = int( len(last_chain_hist) * Propose_kwargs.get("cut_fraction", 0) )
+    last_chain_hist = last_chain_hist[where_to_cut:]
 
     # propose new points
     classifier_name = Propose_kwargs.get("pred_classifier_name", "rbf")
@@ -134,7 +135,7 @@ def get_new_query_points( N_new_points=1, TableData_kwargs = {},
         kappa = Propose_kwargs.get("kappa",150)
         proposed_points, final_kappa = sampler_obj.get_proposed_points(last_chain_hist, N_new_points, kappa,
                                                                  **Propose_kwargs)
-    pred_class, max_probs, where_not_nan = cls_obj.get_class_predictions(classifier_name, proposed_points)
+    pred_class, max_probs, where_not_nan = cls_obj.get_class_predictions(classifier_name, proposed_points, return_ids=False)
     return proposed_points, pred_class
 
 
@@ -165,3 +166,77 @@ def check_dist( original, proposed, threshold = 1e-5):
     above_thresh = distances >= threshold
     proposed_above_thresh_for_all_original = [i.all() for i in above_thresh]
     return np.array(proposed_above_thresh_for_all_original)
+
+
+def get_regular_grid_df(N, jitter=False,verbose=False):
+    """Given N total points, produce an even grid with
+    approximately the same number of evenly spaced points sampled
+    from the analytic data set.
+
+    Parameters
+    ----------
+    N : int
+        Total number of points to make into a 2D even grid
+    jitter : bool, optional
+        Place the center of the grid randomly around (0,0) in the range of one
+        bin width while keeping the span in each axis at 6.
+    verbose : bool, optional
+        Print some diagnostics.
+
+    Returns
+    -------
+    extra_points : pandas DataFrame
+        DataFrame of true data drawn from the analytic classification
+        and regression functions.
+    """
+    root_of_N = np.sqrt(N)
+    x_res = int(root_of_N);  y_res = int(root_of_N)
+    if verbose:
+        print("x_res: {0}, y_res: {1}\nx*y:{2}  |  N:{3}".format(x_res, y_res, x_res*y_res, N ) )
+    if jitter:
+        bin_width = 6 / x_res  # span / num bins
+        random_center = np.random.uniform( low=(-0.5), high=(0.5), size=(2) )
+        center_x = bin_width*random_center[0]; center_y = bin_width*random_center[1]
+    else:
+        center_x = 0; center_y = 0
+    X, Y = np.meshgrid( np.linspace(-3+center_x,3+center_x,x_res),
+                        np.linspace(-3+center_y,3+center_y,y_res) )
+    stacked_points = np.concatenate( (np.vstack(X.flatten()), np.vstack(Y.flatten())), axis=1  )
+
+    from cris.examples.data.analytic_class_regr import get_output
+    class_result, regr_output = get_output( *stacked_points.T )
+
+    extra_points = pd.DataFrame()
+    extra_points["input_1"] = stacked_points.T[0]
+    extra_points["input_2"] = stacked_points.T[1]
+    extra_points["class"] = class_result
+    extra_points["output_1"] = regr_output
+    return extra_points
+
+
+def get_random_grid_df(N):
+    """Given N total points, produce a randomly sampled grid drawn
+    from the analytic data set.
+
+    Parameters
+    ----------
+    N : int
+        Total number of points to drawn from a 2D random data set
+
+    Returns
+    -------
+    random_df : pandas DataFrame
+        DataFrame of true data drawn from the analytic classification
+        and regression functions.
+    """
+    stacked_points = np.random.uniform( low=(-3,-3), high=(3,3), size = (N,2) )
+
+    from cris.examples.data.analytic_class_regr import get_output
+    class_result, regr_output = get_output( *stacked_points.T )
+
+    random_df = pd.DataFrame()
+    random_df["input_1"] = stacked_points.T[0]
+    random_df["input_2"] = stacked_points.T[1]
+    random_df["class"] = class_result
+    random_df["output_1"] = regr_output
+    return random_df
